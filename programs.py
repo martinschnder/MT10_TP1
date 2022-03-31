5730ad2108865f0e56531b8b215c7ab1865791c8
Martin <martin.schneider2001@gmail.com>
2022-03-31 11:02:29 +0200

    avancement

--- programs.py
+++ programs.py
@@ -6,3 +6,31 @@ def test_neutral(t):
                 neutral = false
         if neutral == true:
             return i
+
+def symetric(t):
+    for i in range(len(t)):
+        sym = false
+        for j in range(len(t)):
+            if t[i][j] == t[j][i]:
+                sym = true
+                print((i, j))
+
+def associativity(t):
+    ass = true
+    for i in range(len(t)):
+        for j in range(len(t)):
+            for k in range(len(t)):
+                if t[t[i][j]][k] != t[i][t[j][k]]:
+                    ass = false
+                    break
+    return ass
+
+def morphisme(f, t1, t2):
+    morp = true
+    for i in range(len(t1)):
+        for j in range(len(t1)):
+                if f[t1[i][j]] != t2[f[i]][f[j]]:
+                    morp = false
+                    break
+    return morp
+
